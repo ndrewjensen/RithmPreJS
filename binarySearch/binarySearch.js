70112ec87f5992a9dbbd0a92684d6420d3c18b62
Andrew Jensen <andrewkjensen@gmail.com>
2022-06-30 14:21:51 -0700

    updated binarySearch

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,23 +1,21 @@
 
 function binarySearch(arr, val) {
-  let med = Math.floor(arr.length/2);
-  if (val === arr[med]) {
-    return val;
-  } else if (val < arr[med]) {
-    for (let i = 0; i < med; i++ ) {
-      if (val === arr[i]) {
-        return i;
-      }
+  let left = 0;
+  let right = arr.length - 1
+
+  while (left <= right) {
+    let middle = Math.floor((right + left)/2);
+    if (arr[middle] === val) {
+      return middle;
+    } else if (val < arr[middle]){
+      right = middle -1;
+    } else {
+      left = middle + 1;
     }
-  } else {
-    for (let i = med; i < arr.length; i++ ) {
-      if (val === arr[i]) {
-        return i;
-      }
+
     }
+    return -1;
   }
-  return -1;
-}
 
 
 
